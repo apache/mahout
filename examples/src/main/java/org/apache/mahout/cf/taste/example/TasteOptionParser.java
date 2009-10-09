1970d8829a0290533db5d03c75c40e5c84c0d269
Isabel Drost <isabel@apache.org>
2009-10-09 13:19:57 +0000

    MAHOUT-138 - Missing parser class.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/mahout/trunk@823545 13f79535-47bb-0310-9956-ffa450edef68

---
+++ examples/src/main/java/org/apache/mahout/cf/taste/example/TasteOptionParser.java
@@ -0,0 +1,60 @@
+package org.apache.mahout.cf.taste.example;
+
+import java.io.File;
+
+import org.apache.commons.cli2.CommandLine;
+import org.apache.commons.cli2.Group;
+import org.apache.commons.cli2.Option;
+import org.apache.commons.cli2.OptionException;
+import org.apache.commons.cli2.builder.ArgumentBuilder;
+import org.apache.commons.cli2.builder.DefaultOptionBuilder;
+import org.apache.commons.cli2.builder.GroupBuilder;
+import org.apache.commons.cli2.commandline.Parser;
+import org.apache.mahout.common.CommandLineUtil;
+import org.apache.mahout.common.commandline.DefaultOptionCreator;
+
+/**
+ * This class provides a common implementation for parsing input parameters for
+ * all taste examples. Currently they only need the path to the recommendations
+ * file as input.
+ * 
+ * The class is safe to be used in threaded contexts.
+ */
+public class TasteOptionParser {
+
+  /**
+   * Parse the given command line arguments.
+   * @param args the arguments as given to the application.
+   * @return the input file if a file was given on the command line, null otherwise. 
+   * */
+  public File getRatings(final String[] args) throws OptionException {
+    File file = null;
+    DefaultOptionBuilder obuilder = new DefaultOptionBuilder();
+    ArgumentBuilder abuilder = new ArgumentBuilder();
+    GroupBuilder gbuilder = new GroupBuilder();
+
+    Option inputOpt = obuilder.withLongName("input").withRequired(false)
+        .withShortName("i").withArgument(
+            abuilder.withName("input").withMinimum(1).withMaximum(1).create())
+        .withDescription("The Path for input data directory.").create();
+
+    Option helpOpt = DefaultOptionCreator.helpOption(obuilder);
+
+    Group group = gbuilder.withName("Options").withOption(inputOpt).withOption(
+        helpOpt).create();
+
+    Parser parser = new Parser();
+    parser.setGroup(group);
+    CommandLine cmdLine = parser.parse(args);
+
+    if (cmdLine.hasOption(helpOpt)) {
+      CommandLineUtil.printHelp(group);
+      System.exit(0);
+    }
+
+    String prefsFile = cmdLine.getValue(inputOpt).toString();
+    file = new File(prefsFile);
+    return file;
+  }
+
+}
