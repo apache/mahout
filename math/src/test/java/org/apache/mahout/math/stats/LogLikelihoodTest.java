c21e773c0c8c3b6aa46415fb8a7bcbef6c4885f3
Grant Ingersoll <gsingers@apache.org>
2010-01-05 14:12:09 +0000

    MAHOUT-163: slight refactoring to move log likelihood calculation into math and add tests
    git-svn-id: https://svn.apache.org/repos/asf/lucene/mahout/trunk@896053 13f79535-47bb-0310-9956-ffa450edef68

---
+++ math/src/test/java/org/apache/mahout/math/stats/LogLikelihoodTest.java
@@ -0,0 +1,68 @@
+package org.apache.mahout.math.stats;
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ *
+ *
+ **/
+public class LogLikelihoodTest extends Assert{
+  /*
+  *> entropy(c(1,1))
+[1] 1.386294
+llr(matrix(c(1,0,0,1), nrow=2))
+[1] 2.772589
+llr(matrix(c(10,0,0,10), nrow=2))
+[1] 27.72589
+llr(matrix(c(5,1995,0,100000), nrow=2))
+[1] 39.33052
+llr(matrix(c(1000,1995,1000,100000), nrow=2))
+[1] 4730.737
+llr(matrix(c(1000,1000,1000,100000), nrow=2))
+[1] 5734.343
+llr(matrix(c(1000,1000,1000,99000), nrow=2))
+[1] 5714.932
+*
+   */
+  @Test
+  public void testEntropy() throws Exception {
+
+    assertEquals(LogLikelihood.entropy(1, 1), 1.386294, 0.0001);
+    //TODO: more tests here
+    try {
+      LogLikelihood.entropy(-1, -1);//exception
+      assertFalse(true);
+    } catch (IllegalArgumentException e) {
+      
+    }
+  }
+
+  @Test
+  public void testLogLikelihood() throws Exception {
+    //TODO: check the epsilons
+    assertEquals(LogLikelihood.logLikelihoodRatio(1,0,0,1), 2.772589, 0.0001);
+    assertEquals(LogLikelihood.logLikelihoodRatio(10,0,0,10), 27.72589, 0.0001);
+    assertEquals(LogLikelihood.logLikelihoodRatio(5,1995,0,100000), 39.33052, 0.0001);
+    assertEquals(LogLikelihood.logLikelihoodRatio(1000,1995, 1000, 100000), 4730.737, 0.001);
+    assertEquals(LogLikelihood.logLikelihoodRatio(1000,1000,1000, 100000), 5734.343, 0.001);
+    assertEquals(LogLikelihood.logLikelihoodRatio(1000,1000,1000, 99000), 5714.932, 0.001);
+  }
+
+}
--- utils/src/main/java/org/apache/mahout/utils/vectors/lucene/ClusterLabels.java
+++ utils/src/main/java/org/apache/mahout/utils/vectors/lucene/ClusterLabels.java
@@ -35,6 +35,7 @@ import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.util.OpenBitSet;
 import org.apache.mahout.common.CommandLineUtil;
+import org.apache.mahout.math.stats.LogLikelihood;
 import org.apache.mahout.utils.clustering.ClusterDumper;
 import org.apache.mahout.utils.vectors.TermEntry;
 import org.slf4j.Logger;
@@ -282,35 +283,11 @@ public class ClusterLabels {
     int k12 = clusterSize - inDF;
     int k22 = corpusSize - clusterSize - outDF;
 
-    return logLikelihoodRatio(inDF, k12, outDF, k22);
+    return LogLikelihood.logLikelihoodRatio(inDF, k12, outDF, k22);
   }
 
 
-  private double entropy(int ... elements) {
-    double sum = 0;
-    for (int element : elements) {
-      sum += element;
-    }
-    double result = 0.0;
-    for (int x : elements) {
-      if (x < 0) {
-        throw new IllegalArgumentException("Should not have negative count for entropy computation: (" + x + ")");
-      }
-      int zeroFlag = (x == 0 ? 1 : 0);
-      result += x * Math.log((x + zeroFlag) / sum);
-    }
-    return -result;
-  }
 
-  /**
-   * Calculate Log-likehood ratio for the given matrix.
-   */
-  private double logLikelihoodRatio(int k11, int k12, int k21, int k22) {
-    double rowEntropy = entropy(k11, k12) + entropy(k21, k22);
-    double columnEntropy = entropy(k11, k21) + entropy(k12, k22);
-    double matrixEntropy = entropy(k11, k12, k21, k22);
-    return 2 * (matrixEntropy - rowEntropy - columnEntropy);
-  }
 
   public String getIdField() {
     return idField;
