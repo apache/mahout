65ea79f03dc80e2c55906e1544118b4ec1c3d1a5
Robin Anil <robinanil@apache.org>
2010-01-08 08:23:22 +0000

    MAHOUT-221 Missed out two files while checking in FP-Bonsai
    git-svn-id: https://svn.apache.org/repos/asf/lucene/mahout/trunk@897134 13f79535-47bb-0310-9956-ffa450edef68

---
+++ core/src/test/java/org/apache/mahout/fpm/pfpgrowth/fpgrowth/FrequentPatternMaxHeapTest.java
@@ -0,0 +1,61 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.mahout.fpm.pfpgrowth.fpgrowth;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+import junit.framework.TestCase;
+
+public class FrequentPatternMaxHeapTest extends TestCase {
+
+  public void testMapHeap() {
+    Random gen = new Random(123L);
+
+    FrequentPatternMaxHeap pq = new FrequentPatternMaxHeap(50, true);
+    for (int i = 0; i < 20; i++) {
+      FrequentPatternMaxHeap rs = new FrequentPatternMaxHeap(50, false);
+      for (int j = 0; j < 1000; j++) {
+        Pattern p = generateRandomPattern(gen);
+        rs.insert(p);
+      }
+      for (Pattern p : rs.getHeap()) {
+        pq.insert(p);
+      }
+    }
+  }
+
+  public Pattern generateRandomPattern(Random gen) {
+    int length = 1 + Math.abs(gen.nextInt() % 6);
+    Pattern p = new Pattern();
+    Set<Integer> set = new HashSet<Integer>();
+    for (int i = 0; i < length; i++) {
+      int id = Math.abs(gen.nextInt() % 20);
+      while (set.contains(id)) {
+        id = Math.abs(gen.nextInt() % 20);
+      }
+      set.add(id);
+      int s = 5 + gen.nextInt() % 4;
+      p.add(id, s);
+    }
+    Arrays.sort(p.getPattern());
+    return p;
+  }
+}
