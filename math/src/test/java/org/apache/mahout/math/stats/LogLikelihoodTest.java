530cfbfa496e29e3d939ede323fa4def2fe766d5
Sean R. Owen <srowen@apache.org>
2010-05-22 16:52:05 +0000

    Standardize a few copyrights
    git-svn-id: https://svn.apache.org/repos/asf/mahout/trunk@947317 13f79535-47bb-0310-9956-ffa450edef68

--- math/src/test/java/org/apache/mahout/math/stats/LogLikelihoodTest.java
+++ math/src/test/java/org/apache/mahout/math/stats/LogLikelihoodTest.java
@@ -1,4 +1,3 @@
-package org.apache.mahout.math.stats;
 /**
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
@@ -15,6 +14,8 @@ package org.apache.mahout.math.stats;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+package org.apache.mahout.math.stats;
 
 import org.apache.mahout.math.MahoutTestCase;
 
