705828f78799f586d5539b87307d7dc4031ffef1
Sean R. Owen <srowen@apache.org>
2010-09-05 10:00:33 +0000

    Fixed one instance where tests may not be able to fix a RNG seed.
    git-svn-id: https://svn.apache.org/repos/asf/mahout/trunk@992751 13f79535-47bb-0310-9956-ffa450edef68

--- utils/src/test/java/org/apache/mahout/utils/vectors/text/DictionaryVectorizerTest.java
+++ utils/src/test/java/org/apache/mahout/utils/vectors/text/DictionaryVectorizerTest.java
@@ -44,16 +44,16 @@ public final class DictionaryVectorizerTest extends MahoutTestCase {
   private static final String CHARSET = "abcdef";
   private static final String DELIM = " .,?;:!\t\n\r";
   private static final String ERRORSET = "`1234567890" + "-=~@#$%^&*()_+[]{}'\"/<>|\\";
-  private static final Random random = RandomUtils.getRandom();
 
+  private final Random random = RandomUtils.getRandom();
   private FileSystem fs;
 
-  private static char getRandomDelimiter() {
-    return DELIM.charAt(random.nextInt(DictionaryVectorizerTest.DELIM.length()));
+  private char getRandomDelimiter() {
+    return DELIM.charAt(random.nextInt(DELIM.length()));
   }
 
-  private static String getRandomDocument() {
-    int length = (AVG_DOCUMENT_LENGTH >> 1) + DictionaryVectorizerTest.random.nextInt(AVG_DOCUMENT_LENGTH);
+  private String getRandomDocument() {
+    int length = (AVG_DOCUMENT_LENGTH >> 1) + random.nextInt(AVG_DOCUMENT_LENGTH);
     StringBuilder sb = new StringBuilder(length * AVG_SENTENCE_LENGTH * AVG_WORD_LENGTH);
     for (int i = 0; i < length; i++) {
       sb.append(getRandomSentence());
@@ -61,8 +61,8 @@ public final class DictionaryVectorizerTest extends MahoutTestCase {
     return sb.toString();
   }
 
-  private static String getRandomSentence() {
-    int length = (AVG_SENTENCE_LENGTH >> 1) + DictionaryVectorizerTest.random.nextInt(AVG_SENTENCE_LENGTH);
+  private String getRandomSentence() {
+    int length = (AVG_SENTENCE_LENGTH >> 1) + random.nextInt(AVG_SENTENCE_LENGTH);
     StringBuilder sb = new StringBuilder(length * AVG_WORD_LENGTH);
     for (int i = 0; i < length; i++) {
       sb.append(getRandomString()).append(' ');
@@ -71,16 +71,14 @@ public final class DictionaryVectorizerTest extends MahoutTestCase {
     return sb.toString();
   }
 
-  private static String getRandomString() {
-    int length = (AVG_WORD_LENGTH >> 1) + DictionaryVectorizerTest.random.nextInt(AVG_WORD_LENGTH);
+  private String getRandomString() {
+    int length = (AVG_WORD_LENGTH >> 1) + random.nextInt(AVG_WORD_LENGTH);
     StringBuilder sb = new StringBuilder(length);
     for (int i = 0; i < length; i++) {
-      sb.append(DictionaryVectorizerTest.CHARSET.charAt(
-          DictionaryVectorizerTest.random.nextInt(DictionaryVectorizerTest.CHARSET.length())));
+      sb.append(CHARSET.charAt(random.nextInt(CHARSET.length())));
     }
-    if (DictionaryVectorizerTest.random.nextInt(10) == 0) {
-      sb.append(DictionaryVectorizerTest.ERRORSET.charAt(
-          DictionaryVectorizerTest.random.nextInt(DictionaryVectorizerTest.ERRORSET.length())));
+    if (random.nextInt(10) == 0) {
+      sb.append(ERRORSET.charAt(random.nextInt(ERRORSET.length())));
     }
     return sb.toString();
   }
