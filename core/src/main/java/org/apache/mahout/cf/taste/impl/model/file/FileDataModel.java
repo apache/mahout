0c4bba06474f7d7b4d5e93ced45ea665b296fa75
Sean R. Owen <srowen@apache.org>
2011-01-10 22:27:00 +0000

    MAHOUT-581
    git-svn-id: https://svn.apache.org/repos/asf/mahout/trunk@1057402 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/main/java/org/apache/mahout/cf/taste/impl/model/file/FileDataModel.java
+++ core/src/main/java/org/apache/mahout/cf/taste/impl/model/file/FileDataModel.java
@@ -368,7 +368,9 @@ public class FileDataModel extends AbstractDataModel {
       return;
     }
 
-    String[] tokens = delimiterPattern.split(line);
+    // Consume up to 4 tokens, and gather whatever is left in an unused 5th token:
+    String[] tokens = delimiterPattern.split(line, 5);
+
     Preconditions.checkArgument(tokens.length >= 3, "Bad line: %s", line);
 
     String userIDString = tokens[0];
