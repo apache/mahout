0b04779565a30f0e1e6debb6c2ce846fb1df1447
Sean R. Owen <srowen@apache.org>
2011-02-15 07:57:39 +0000

    MAHOUT-604 avoid an NPE by updating Preconditions check
    git-svn-id: https://svn.apache.org/repos/asf/mahout/trunk@1070798 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/main/java/org/apache/mahout/classifier/ConfusionMatrix.java
+++ core/src/main/java/org/apache/mahout/classifier/ConfusionMatrix.java
@@ -91,20 +91,20 @@ public class ConfusionMatrix implements Summarizable {
   }
   
   public int getCount(String correctLabel, String classifiedLabel) {
-    Preconditions.checkArgument(!labelMap.containsKey(correctLabel)
-        || labelMap.containsKey(classifiedLabel)
-        || defaultLabel.equals(classifiedLabel),
-        "Label not found " + correctLabel + ' ' + classifiedLabel);
+    Preconditions.checkArgument(labelMap.containsKey(correctLabel),
+                                "Label not found: " + correctLabel);
+    Preconditions.checkArgument(labelMap.containsKey(classifiedLabel),
+                                "Label not found: " + classifiedLabel);
     int correctId = labelMap.get(correctLabel);
     int classifiedId = labelMap.get(classifiedLabel);
     return confusionMatrix[correctId][classifiedId];
   }
   
   public void putCount(String correctLabel, String classifiedLabel, int count) {
-    Preconditions.checkArgument(!labelMap.containsKey(correctLabel)
-        || labelMap.containsKey(classifiedLabel)
-        || defaultLabel.equals(classifiedLabel),
-        "Label not found " + correctLabel + ' ' + classifiedLabel);
+    Preconditions.checkArgument(labelMap.containsKey(correctLabel),
+                                "Label not found: " + correctLabel);
+    Preconditions.checkArgument(labelMap.containsKey(classifiedLabel),
+                                "Label not found: " + classifiedLabel);
     int correctId = labelMap.get(correctLabel);
     int classifiedId = labelMap.get(classifiedLabel);
     confusionMatrix[correctId][classifiedId] = count;
