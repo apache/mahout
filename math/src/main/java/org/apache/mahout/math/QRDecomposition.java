5212d25bd0b371bd2fab261d8bcf722b3eb0b50e
Ted Dunning <tdunning@apache.org>
2011-09-01 11:28:29 +0000

    MAHOUT-790 - Fixed copyright and license on QRDecomposition and SVDDecomposition
    git-svn-id: https://svn.apache.org/repos/asf/mahout/trunk@1164013 13f79535-47bb-0310-9956-ffa450edef68

--- math/src/main/java/org/apache/mahout/math/QRDecomposition.java
+++ math/src/main/java/org/apache/mahout/math/QRDecomposition.java
@@ -13,16 +13,14 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * Copyright 1999 CERN - European Organization for Nuclear Research.
+ * Permission to use, copy, modify, distribute and sell this software and its documentation for any purpose
+ * is hereby granted without fee, provided that the above copyright notice appear in all copies and
+ * that both that copyright notice and this permission notice appear in supporting documentation.
+ * CERN makes no representations about the suitability of this software for any purpose.
+ * It is provided "as is" without expressed or implied warranty.
  */
-
-/*
-Copyright ? 1999 CERN - European Organization for Nuclear Research.
-Permission to use, copy, modify, distribute and sell this software and its documentation for any purpose
-is hereby granted without fee, provided that the above copyright notice appear in all copies and
-that both that copyright notice and this permission notice appear in supporting documentation.
-CERN makes no representations about the suitability of this software for any purpose.
-It is provided "as is" without expressed or implied warranty.
-*/
 package org.apache.mahout.math;
 
 import org.apache.mahout.math.function.Functions;
