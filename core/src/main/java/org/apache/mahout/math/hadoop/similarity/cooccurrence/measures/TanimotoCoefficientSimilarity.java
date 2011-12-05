ce91968a54263986f45f74dac55f8987ec6941ed
Sean R. Owen <srowen@apache.org>
2011-12-05 20:13:43 +0000

    MAHOUT-902 oops do not return NaN from distributed item-item simliarity as 0 means 'ignore'
    git-svn-id: https://svn.apache.org/repos/asf/mahout/trunk@1210603 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/main/java/org/apache/mahout/math/hadoop/similarity/cooccurrence/measures/TanimotoCoefficientSimilarity.java
+++ core/src/main/java/org/apache/mahout/math/hadoop/similarity/cooccurrence/measures/TanimotoCoefficientSimilarity.java
@@ -21,7 +21,8 @@ public class TanimotoCoefficientSimilarity extends CountbasedMeasure {
 
   @Override
   public double similarity(double dots, double normA, double normB, int numberOfColumns) {
-    return dots == 0 ? Double.NaN : dots / (normA + normB - dots);
+    // Return 0 even when dots == 0 since this will cause it to be ignored -- not NaN
+    return dots / (normA + normB - dots);
   }
 
   @Override
