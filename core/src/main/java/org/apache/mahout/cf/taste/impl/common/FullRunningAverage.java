b89d1c35301bd6032a388b429c7e699d615dd6ac
sslavic <sslavic@unknown>
2013-08-13 21:46:24 +0000

    NOJIRA Fixed typo in javadoc
    git-svn-id: https://svn.apache.org/repos/asf/mahout/trunk@1513664 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/main/java/org/apache/mahout/cf/taste/impl/common/FullRunningAverage.java
+++ core/src/main/java/org/apache/mahout/cf/taste/impl/common/FullRunningAverage.java
@@ -21,7 +21,7 @@ import java.io.Serializable;
 
 /**
  * <p>
- * A simple class that can keep track of a running avearage of a series of numbers. One can add to or remove
+ * A simple class that can keep track of a running average of a series of numbers. One can add to or remove
  * from the series, as well as update a datum in the series. The class does not actually keep track of the
  * series of values, just its running average, so it doesn't even matter if you remove/change a value that
  * wasn't added.
