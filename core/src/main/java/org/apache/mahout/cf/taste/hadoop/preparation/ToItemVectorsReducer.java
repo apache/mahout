f868a68b7eb416a6eab16c1bc5111a9304abda78
Sebastian Schelter <ssc@apache.org>
2013-08-27 04:34:12 +0000

    MAHOUT-1318 Make ToItemVectorsReducer public
    git-svn-id: https://svn.apache.org/repos/asf/mahout/trunk@1517747 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/main/java/org/apache/mahout/cf/taste/hadoop/preparation/ToItemVectorsReducer.java
+++ core/src/main/java/org/apache/mahout/cf/taste/hadoop/preparation/ToItemVectorsReducer.java
@@ -23,12 +23,14 @@ import org.apache.mahout.math.VectorWritable;
 
 import java.io.IOException;
 
-class ToItemVectorsReducer extends Reducer<IntWritable,VectorWritable,IntWritable,VectorWritable> {
+public class ToItemVectorsReducer extends Reducer<IntWritable,VectorWritable,IntWritable,VectorWritable> {
 
   private final VectorWritable merged = new VectorWritable();
+
   @Override
   protected void reduce(IntWritable row, Iterable<VectorWritable> vectors, Context ctx)
     throws IOException, InterruptedException {
+
     merged.setWritesLaxPrecision(true);
     merged.set(VectorWritable.mergeToVector(vectors.iterator()));
     ctx.write(row, merged);
