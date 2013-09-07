3828ebf93ec969d4a7f819383025bfb91ad9a741
sslavic <sslavic@unknown>
2013-09-07 18:42:14 +0000

    Fixed typos in comment and JavaDoc
    git-svn-id: https://svn.apache.org/repos/asf/mahout/trunk@1520787 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/main/java/org/apache/mahout/cf/taste/impl/similarity/AbstractSimilarity.java
+++ core/src/main/java/org/apache/mahout/cf/taste/impl/similarity/AbstractSimilarity.java
@@ -93,7 +93,7 @@ abstract class AbstractSimilarity extends AbstractItemSimilarity implements User
    * @param n
    *          total number of users or items
    * @param sumXY
-   *          sum of product of user/item preference values, over all items/users prefererred by both
+   *          sum of product of user/item preference values, over all items/users preferred by both
    *          users/items
    * @param sumX2
    *          sum of the square of user/item preference values, over the first item/user
@@ -102,7 +102,7 @@ abstract class AbstractSimilarity extends AbstractItemSimilarity implements User
    * @param sumXYdiff2
    *          sum of squares of differences in X and Y values
    * @return similarity value between -1.0 and 1.0, inclusive, or {@link Double#NaN} if no similarity can be
-   *         computed (e.g. when no items have been rated by both uesrs
+   *         computed (e.g. when no items have been rated by both users
    */
   abstract double computeResult(int n, double sumXY, double sumX2, double sumY2, double sumXYdiff2);
   
@@ -246,7 +246,7 @@ abstract class AbstractSimilarity extends AbstractItemSimilarity implements User
     double sumXYdiff2 = 0.0;
     int count = 0;
     
-    // No, pref inferrers and transforms don't appy here. I think.
+    // No, pref inferrers and transforms don't apply here. I think.
     
     while (true) {
       int compare = xIndex < yIndex ? -1 : xIndex > yIndex ? 1 : 0;
