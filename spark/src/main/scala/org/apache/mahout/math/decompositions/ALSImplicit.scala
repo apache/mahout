288a21edd01435cd7f1b420513941792d61e19ec
Dmitriy Lyubimov <dlyubimov@apache.org>
2014-06-04 18:19:52 -0700

    ALS WR in WIP

--- spark/src/main/scala/org/apache/mahout/math/decompositions/ALSImplicit.scala
+++ spark/src/main/scala/org/apache/mahout/math/decompositions/ALSImplicit.scala
@@ -5,9 +5,10 @@ import scalabindings._
 import RLikeOps._
 import drm._
 import RLikeDrmOps._
-import scala.reflect.ClassTag
+import org.apache.mahout.sparkbindings._
 import scala.util.Random
-import org.apache.spark.graphx._
+import scala.collection.JavaConversions._
+import org.apache.spark.SparkContext._
 
 /**
  *
@@ -28,7 +29,7 @@ object ALSImplicit {
     // cbind(U,A):
     var drmUA = drmA.mapBlock(ncol = k + drmA.ncol) {
       case (keys, block) =>
-        val uaBlock = new SparseRowMatrix(block.nrow, block.ncol + k)
+        val uaBlock = block.like(block.nrow, block.ncol + k)
         uaBlock(::, 0 until k) := Matrices.symmetricUniformView(uaBlock.nrow, k, Random.nextInt()) * 0.01
         uaBlock(::, k until uaBlock.ncol) := block
         keys -> uaBlock
@@ -37,19 +38,31 @@ object ALSImplicit {
     // cbind(V,A'):
     val drmVAt = drmAt.mapBlock(ncol = k + drmAt.ncol) {
       case (keys, block) =>
-        val vatBlock = new SparseRowMatrix(block.nrow, block.ncol + k)
+        val vatBlock = block.like(block.nrow, block.ncol + k)
         vatBlock(::, k until vatBlock.ncol) := block
         keys -> vatBlock
     }
+    
+    var i = 0 
+    var stop = false
+    while (i < maxIter && ! stop) {
+      // update VAt, TODO to be ctd.
+      drmVAt.rdd.cogroup(other=generateMessages(drmUA,k)).map()
+
+      i+= 1 
+    }
   }
 
-  private def buildGraph[K:ClassTag](drmInput:DrmLike[K]) = {
-    // Build preference 1 edges (indicator matrix where confidence > 0)
-//    val edges = drmInput.m
+  private def generateMessages(drmUA:DrmLike[Int], k:Int) = {
 
-    // Graph()
-
-
+    val n = drmUA.ncol
+    // Now we delve into Spark-specific processing.
+    drmUA.rdd.flatMap{
+      case (rowKey, row) =>
+      val uvec = new DenseVector(k) := row(0 until k)
+      val cvec = row(k until n)
+      cvec.nonZeroes().map(_.index()-> uvec)
+    }
   }
 
 }
