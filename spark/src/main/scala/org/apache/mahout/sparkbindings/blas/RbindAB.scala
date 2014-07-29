846307d5b03ef7df6d3c890f2a291b4c11581e80
Dmitriy Lyubimov <dlyubimov@apache.org>
2014-07-29 13:17:15 -0700

    MAHOUT-1596: implement rbind() operator
    This closes apache/mahout#30
    
    Conflicts:
    	math-scala/src/test/scala/org/apache/mahout/math/drm/RLikeDrmOpsSuiteBase.scala

---
+++ spark/src/main/scala/org/apache/mahout/sparkbindings/blas/RbindAB.scala
@@ -0,0 +1,49 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.mahout.sparkbindings.blas
+
+import org.apache.log4j.Logger
+import scala.reflect.ClassTag
+import org.apache.mahout.sparkbindings.drm.DrmRddInput
+import org.apache.mahout.math.drm.logical.OpRbind
+
+/** Physical `rbind` */
+object RbindAB {
+
+  private val log = Logger.getLogger(RbindAB.getClass)
+
+  def rbindAB[K: ClassTag](op: OpRbind[K], srcA: DrmRddInput[K], srcB: DrmRddInput[K]): DrmRddInput[K] = {
+
+    // If any of the inputs is blockified, use blockified inputs
+    if (srcA.isBlockified || srcB.isBlockified) {
+      val a = srcA.toBlockifiedDrmRdd()
+      val b = srcB.toBlockifiedDrmRdd()
+
+      // Union seems to be fine, it is indeed just do partition-level unionization, no shuffles
+      new DrmRddInput(blockifiedSrc = Some(a ++ b))
+
+    } else {
+
+      // Otherwise, use row-wise inputs -- no reason to blockify here.
+      val a = srcA.toDrmRdd()
+      val b = srcB.toDrmRdd()
+
+      new DrmRddInput(rowWiseSrc = Some(op.ncol -> (a ++ b)))
+    }
+  }
+}
