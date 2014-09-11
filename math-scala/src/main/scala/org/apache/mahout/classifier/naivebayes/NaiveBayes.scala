bc462fd925004c33a04f1d2fea387978999cf25f
Andrew Palumbo <ap.dev@outlook.com>
2014-09-10 22:28:16 -0400

    First attempt at combiner for seq2sparse outputted vectors

--- math-scala/src/main/scala/org/apache/mahout/classifier/naivebayes/NaiveBayes.scala
+++ math-scala/src/main/scala/org/apache/mahout/classifier/naivebayes/NaiveBayes.scala
@@ -1,12 +1,19 @@
 package org.apache.mahout.classifier.naivebayes
 
+import java.util
+
 import org.apache.mahout.math._
 import org.apache.mahout.math.function.VectorFunction
+import org.apache.mahout.math.map.{OpenIntObjectHashMap, OpenObjectIntHashMap}
 import scalabindings._
 import scalabindings.RLikeOps._
 import drm.RLikeDrmOps._
 import drm._
 import scala.reflect.ClassTag
+import scala.language.asInstanceOf
+import collection._
+import JavaConversions._
+
 
 import org.apache.mahout.classifier.naivebayes.training.ComplementaryThetaTrainer
 
@@ -64,6 +71,102 @@ object NaiveBayes {
       thetaNormalizer, alphaI, trainComplementary)
 
   }
+
+  /** extract label Keys from raw TF/TF-IDF Matrix
+    *
+    * @param stringKeyedObservations DrmLike matrix; Output from seq2sparse
+    *                                in form K= /Category/document_title
+    *                                        V= TF or TF-IDF values per term
+    * @return (labelIndexMap,aggregatedByLabelObservationDrm)
+    *           labelIndexMap is an OpenObjectIntHashMap K= label index
+    *                                                  V= label
+    *           aggregatedByLabelObservationDrm is a DrmLike[Int] of aggregated
+    *             TF or TF-IDF counts per label
+    */
+
+
+
+  def extractLabelsAndAggregateObservations( stringKeyedObservations: DrmLike[String] ):
+  (mutable.HashMap[Integer,String], DrmLike[Int]) = {
+
+    // get the label row keys as
+    val rowLabelBindings = stringKeyedObservations.getRowLabelBindings
+
+    // extract categories from labels assigned by seq2sparse
+    // Categories are Stored in Drm Keys as: /Category/document_id
+    val labelVectorByRowIndex = new util.Vector[String](rowLabelBindings.size())
+    val labelMapByRowIndex = new mutable.HashMap[Integer,String]
+    for ((key, value) <- rowLabelBindings) {
+      labelVectorByRowIndex.set(value, key.split("/")(1))
+      labelMapByRowIndex.put(value, key.split("/")(1) )
+    }
+
+    // convert to an IntKeyed Drm
+    // must be a better way to do this.
+    val intKeyedObservations = drmParallelizeEmpty(
+                            stringKeyedObservations.nrow.toInt,
+                            stringKeyedObservations.ncol)
+    for (i <- stringKeyedObservations.nrow.toInt) {
+      for ( j <-stringKeyedObservations.ncol) {
+        intKeyedObservations.set(i,
+                                 j,
+                                 stringKeyedObservations.get(i,j))
+      }
+    }
+
+    // get rid of stringKeyedObservations - we dont need them anymore
+    stringKeyedObservations.uncache
+
+    var categoryIndex = 0.0d
+    val encodedCategoryByKey = new mutable.HashMap[String,Integer]
+    val encodedCategoryByRowIndexVector = new DenseVector(labelVectorByRowIndex.size)
+
+    // encode rows as an integer so we can broadcast as a vector
+    for (i <- 0 until labelVectorByRowIndex.size) {
+      if (!encodedCategoryByKey.contains()) {
+        encodedCategoryByRowIndexVector.set(i, categoryIndex)
+        encodedCategoryByKey.put(labelVectorByRowIndex.get(i), i)
+        categoryIndex += 1.0
+      } else {
+        encodedCategoryByRowIndexVector.set(i ,
+                              encodedCategoryByKey
+                                .getOrElse(labelVectorByRowIndex.get(i), -1)
+                                .asInstanceOf[Double])
+      }
+    }
+
+    // "Combiner": Map and aggregate by Category. Do this by
+    // broadcasting the encoded category vector and mapping
+    // a transposed IntKeyed Drm out so that all categories
+    // will be present on all nodes as columns and can be referenced
+    // by BCastEncodedCategoryByRowVector.  Iteratively sum all categories
+
+    val ncategories = categoryIndex.toInt
+
+    val BCastEncodedCategoryByRowVector= drmBroadcast(encodedCategoryByRowIndexVector)
+
+    val aggregetedObservationByLabelDrm = intKeyedObservations.t.mapBlock(ncol = ncategories) {
+      case (keys, blockA) =>
+        val blockB = new DenseMatrix(keys.size, ncategories)
+        //val blockB = blockA.zeroes(keys.size, ncategories)
+        var category : Int = 0
+
+        for (i <- 0 until blockA.numRow) {
+          for (j <- 0 until ncol) {
+            category=BCastEncodedCategoryByRowVector.get(j)
+            blockB.set(i, category, (blockB.get(i,category) + blockA.get(i,j)))
+          }
+        }
+        keys -> blockB
+    }
+
+    // Now return the labelMapByRowIndex HashMap and the the transpose of
+    // aggregetedObservationDrm which can be used as input to trainNB
+
+    (labelMapByRowIndex, aggregetedObservationByLabelDrm.t)
+  }
+
+
 //  private def vectorSumFunc = new VectorFunction {
 //    def apply(f: Vector): Double = f.sum
 //  }
