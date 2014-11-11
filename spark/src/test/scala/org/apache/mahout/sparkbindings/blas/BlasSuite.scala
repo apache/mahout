060d6f136343bb659902bfed64b93fbc58c9dcfe
Gokhan <gkhncpn@gmail.com>
2014-11-11 22:40:27 +0200

    making sure that zeroval is a double in tests

--- spark/src/test/scala/org/apache/mahout/sparkbindings/blas/BlasSuite.scala
+++ spark/src/test/scala/org/apache/mahout/sparkbindings/blas/BlasSuite.scala
@@ -155,27 +155,30 @@ class BlasSuite extends FunSuite with DistributedSparkSuite {
     import RLikeDrmOps._
 
     val inCoreA = dense((1, 2, 3), (2, 3, 4), (3, 4, 5))
-    val accControl = inCoreA.sum
 
     val drmA = drmParallelize(m = inCoreA, numPartitions = 2)
 
+    val accControl = drmA.zSum
+
     val seqOp: (Double, (Array[Int], _<: Matrix)) => Double = (value, matrix) =>
-      value + matrix._2.sum
+      value + matrix._2.zSum
 
     val combOp: (Double, Double) => Double = (val1, val2) => val1 + val2
 
     //check if it accumulates correctly
     val acc1 = drmA.aggregateBlocks(0.0, seqOp, combOp)
 
-    //check if it can be the last element of a chain of matrix ops
-    val acc2 = ((drmA + 1) * 1 - 1).aggregateBlocks(0, seqOp, combOp)
+    //check if it can be the called after a series of matrix ops
+    val drmB = drmA + 1
+    val accControl2 = drmB.zSum()
+    val acc2 = drmB.aggregateBlocks(0.0, seqOp, combOp)
 
 
     println(acc1)
     println(acc2)
 
     assert(acc1 == accControl)
-    assert(acc2 == accControl)
+    assert(acc2 == accControl2)
   }
 
 }
