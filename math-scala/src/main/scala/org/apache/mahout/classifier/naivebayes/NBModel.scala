737745d3853d459354c78913600d18b43a939e2f
Andrew Palumbo <ap.dev@outlook.com>
2014-11-20 17:17:13 -0500

    Use row slicing and nonZero iterators.  Clean Up

---
+++ math-scala/src/main/scala/org/apache/mahout/classifier/naivebayes/NBModel.scala
@@ -0,0 +1,20 @@
+package org.apache.mahout.classifier.naivebayes
+
+//import org.apache.mahout.math._
+//
+//import org.apache.mahout.math.{drm, scalabindings}
+//
+//import scalabindings._
+//import scalabindings.RLikeOps._
+//import drm.RLikeDrmOps._
+//import drm._
+//import scala.reflect.ClassTag
+//import scala.language.asInstanceOf
+//import collection._
+//import JavaConversions._
+//
+//
+//
+//class NBModel extends NaiveBayesModel {
+//
+//}
