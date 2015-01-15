a48c3d97a7811d96324d85171a3527fbf763c082
3110101637 <2281784755@qq.com>
2015-01-15 22:59:24 +0900

    lll

--- math/src/main/java/org/apache/mahout/math/mf/PMF_SGD.java
+++ math/src/main/java/org/apache/mahout/math/mf/PMF_SGD.java
@@ -4,7 +4,6 @@ import org.apache.mahout.math.DenseMatrix;
 import org.apache.mahout.math.Matrix;
 import org.apache.mahout.math.function.Functions;
 
-<<<<<<< HEAD
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -22,32 +21,6 @@ public class PMF_SGD implements MF {
     private final double alpha;
     private final double lambdap;
     private final double lambdaq;
-=======
-/**
- * Created by lmq on 2014/12/24.
- *
- * Probabilistic Matrix Factorization.
- * PMF_SGD: Class for Probabilistic Matrix Factorization using stotastic gradient descent
- * [1] Mnih, Andriy, and Ruslan Salakhutdinov. "Probabilistic matrix factorization." Advances in neural information processing systems. 2007.
- *
- *
- * Probabilistic Matrix Factorization. Factorize a data matrix into two matrices
- * s.t. R = | data - P*Q | = | is minimal.
- * Uses stochastic gradient descent
- *
- */
-
-public class PMF_SGD implements MF {
-
-    /** Arrays for internal storage of V, W and H. */
-    private final Matrix R;
-    private final Matrix P;
-    private final Matrix Q;
-
-    private final double alpha;
-    private final double beta1;
-    private final double beta2;
->>>>>>> 324ac412b16823924f660db04425309d097068c3
 
     /** Row and column dimensions. */
     private final int n;
@@ -71,11 +44,7 @@ public class PMF_SGD implements MF {
     private final double errMax;
 
 
-<<<<<<< HEAD
     public PMF_SGD(Matrix arg, int r, double alpha, double lambdap, double lambdaq, int stepNum, double errMax) {
-=======
-    public PMF_SGD(Matrix arg, int r, double alpha, double beta1, double beta2, int stepNum, double errMax) {
->>>>>>> 324ac412b16823924f660db04425309d097068c3
         this.n = arg.numRows();
         this.m = arg.numCols();
         this.r = r;
@@ -83,7 +52,6 @@ public class PMF_SGD implements MF {
         this.stepMax = stepNum;
         this.errMax = errMax;
         this.alpha = alpha;
-<<<<<<< HEAD
         this.lambdap = lambdap;
         this.lambdaq = lambdaq;
 
@@ -131,32 +99,6 @@ public class PMF_SGD implements MF {
             out.write(str);
             out.flush();*/
 
-=======
-        this.beta1 = beta1;
-        this.beta2 = beta2;
-
-        this.P = new DenseMatrix(n, r).assign(Functions.random());
-        this.Q = new DenseMatrix(r, m).assign(Functions.random());
-        this.R = arg;
-
-        this.object = calObject(R, P, Q);
-    }
-
-    //@Override
-    public void solve() {
-        for(; stepNum < stepMax; stepNum++) {
-            for(int i=0; i<n; i++) for(int j=0; j<m; j++) {
-                if (R.get(i, j) != 0) {
-                    double eij = R.get(i, j) - P.viewRow(i).dot(Q.viewColumn(j));
-                    for (int k = 0; k < r; k++) {
-                        P.set(i, k, P.get(i, k) + alpha * (eij * Q.get(k, j) - beta1 * P.get(i, k)));
-                        Q.set(k, j, Q.get(k, j) + alpha * (eij * P.get(i, k) - beta2 * Q.get(k, j)));
-                    }
-                }
-            }
-
-            double newObject = calObject(R, P, Q);
->>>>>>> 324ac412b16823924f660db04425309d097068c3
 
             if(Math.abs(object - newObject) < errMax) {
                 object = newObject;
@@ -165,36 +107,12 @@ public class PMF_SGD implements MF {
             object = newObject;
         }
 
-<<<<<<< HEAD
 
        /* } catch (IOException e) {
             e.printStackTrace();
         }*/
 
 
-=======
-    }
-
-    @Override
-    public double calObject(Matrix R, Matrix P, Matrix Q) {
-        Matrix PQ = (P.times(Q));
-        double err = 0;
-        for(int i=0; i<n; i++) for(int j=0; j<m; j++)
-            if (R.get(i, j) != 0) err += Math.pow(R.get(i, j) - PQ.get(i, j), 2);
-        for(int i=0; i<n; i++) for(int j=0; j<r; j++) err += (beta1) * (Math.pow(P.get(i, j), 2));
-        for(int i=0; i<r; i++) for(int j=0; j<m; j++) err += (beta2) * (Math.pow(Q.get(i, j), 2));
-        return err/2;
-    }
-
-    @Override
-    public Matrix getW() {
-        return P;
-    }
-
-    @Override
-    public Matrix getH() {
-        return Q;
->>>>>>> 324ac412b16823924f660db04425309d097068c3
     }
 
     @Override
@@ -202,7 +120,6 @@ public class PMF_SGD implements MF {
         return stepNum;
     }
 
-<<<<<<< HEAD
     //@Override
     private double calObject(Matrix V, Matrix W, Matrix H) {
         Matrix WH = (W.times(H));
@@ -217,10 +134,6 @@ public class PMF_SGD implements MF {
 
     @Override
     public double getObjectFunctionValue() {
-=======
-    @Override
-    public double getObjectFunctionValue(){
->>>>>>> 324ac412b16823924f660db04425309d097068c3
         return object;
     }
 }
