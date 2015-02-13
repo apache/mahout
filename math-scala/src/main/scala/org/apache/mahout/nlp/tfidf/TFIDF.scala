4be1d9f208818b8c5fb8223be80cfc27dd8316eb
Andrew Palumbo <apalumbo@apache.org>
2015-02-12 20:44:41 -0500

    Basic implementation of TF-IDF classes

---
+++ math-scala/src/main/scala/org/apache/mahout/nlp/tfidf/TFIDF.scala
@@ -0,0 +1,69 @@
+package org.apache.mahout.nlp.tfidf
+
+import org.apache.lucene.search.similarities.DefaultSimilarity
+
+trait Weight {
+  /**
+   * @param tf term freq
+   * @param df doc freq
+   * @param length Length of the document
+   * @param numDocs the total number of docs
+   * @return The weight
+   */
+  def calculate(tf: Int, df: Int, length: Int, numDocs: Int): Double
+}
+
+
+class TFIDF extends Weight {
+  private final val sim: DefaultSimilarity = new DefaultSimilarity
+
+  /**
+   * Calculate TF-IDF weight
+   *
+   * Lucene DefaultSimilarity's TF-IDF calculation uses the formula:
+   *
+   *   sqrt(termFreq) * log((numDocs / (docFreq + 1)) + 1.0)
+   *
+   * Note: this is consistent with the MapReduce seq2sparse implementation of TF-IDF weights
+   * and is slightly different from Spark's TD-IDF calculation which is implemented as:
+   *
+   *   termFreq * log((numDocs + 1.0) / (docFreq + 1.0))
+   *
+   *
+   * @param tf term freq
+   * @param df doc freq
+   * @param length Length of the document - UNUSED
+   * @param numDocs the total number of docs
+   * @return The TF-IDF weight as calculated by DefaultSimilarity
+   */
+  def calculate(tf: Int, df: Int, length: Int, numDocs: Int): Double = {
+
+    // Lucene DefaultSimilarity's TF-IDF is implemented as:
+    // sqrt(tf) * log(numDocs/(df+1)) + 1
+    sim.tf(tf) * sim.idf(df, numDocs)
+  }
+}
+
+class TF extends Weight {
+
+  /**
+   * For TF Weight simply return the absolute TF.  We do not use the DefaultSimilarity's TF-IDF calculation
+   * which returns:
+   *
+   *  sqrt(tf)
+   *
+   * this is consistent with the MapReduce seq2sparse implementation of TF-IDF weights.
+   *
+   * @param tf term freq
+   * @param df doc freq - UNUSED
+   * @param length Length of the document - UNUSED
+   * @param numDocs the total number of docs - UNUSED
+   * based on term frequency only - UNUSED
+   * @return The weight = tf param
+   */
+  def calculate(tf: Int, df: Int = -Int.MaxValue, length: Int = -Int.MaxValue, numDocs: Int = -Int.MaxValue): Double = {
+    tf
+  }
+}
+
+
