785668906b5e69db6c9352ffba961b49bda450d2
smarthi <smarthi@apache.org>
2015-08-08 14:35:41 -0400

    MAHOUT-1642 Iterator class within SimilarItems class always misses the first element, this closes apache/mahout#134

--- mr/src/main/java/org/apache/mahout/cf/taste/similarity/precompute/SimilarItems.java
+++ mr/src/main/java/org/apache/mahout/cf/taste/similarity/precompute/SimilarItems.java
@@ -65,7 +65,7 @@ public class SimilarItems {
 
   private class SimilarItemsIterator extends UnmodifiableIterator<SimilarItem> {
 
-    private int index = 0;
+    private int index = -1;
 
     @Override
     public boolean hasNext() {
