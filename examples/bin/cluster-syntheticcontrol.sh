da65cbe2b745da98a39ef51723154ef4a450e832
Andrew Musselman <akm@apache.org>
2016-03-17 18:00:01 -0700

    MAHOUT-1773: Fix cluster-syntheticcontrol.sh for HDFS synthax closes apache/mahout#157

--- examples/bin/cluster-syntheticcontrol.sh
+++ examples/bin/cluster-syntheticcontrol.sh
@@ -66,13 +66,13 @@ if [ ! -f ${WORK_DIR}/synthetic_control.data ]; then
 fi
 if [ "$HADOOP_HOME" != "" ] && [ "$MAHOUT_LOCAL" == "" ]; then
   echo "Checking the health of DFS..."
-  $DFS -ls
+  $DFS -ls /
   if [ $? -eq 0 ];then 
     echo "DFS is healthy... "
     echo "Uploading Synthetic control data to HDFS"
-    $DFSRM testdata
-    $DFS -mkdir testdata
-    $DFS -put ${WORK_DIR}/synthetic_control.data testdata
+    $DFSRM ${WORK_DIR}/testdata
+    $DFS -mkdir ${WORK_DIR}/testdata
+    $DFS -put ${WORK_DIR}/synthetic_control.data ${WORK_DIR}/testdata
     echo "Successfully Uploaded Synthetic control data to HDFS "
 
     ../../bin/mahout org.apache.mahout.clustering.syntheticcontrol."${clustertype}".Job
