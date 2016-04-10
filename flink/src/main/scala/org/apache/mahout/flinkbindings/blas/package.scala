430310dbf7870e263ea0340df9b226655ce82a72
smarthi <smarthi@apache.org>
2016-04-09 23:04:53 -0400

    MAHOUT-1814:Implement drm2intKeyed in flink bindings, this closes apache/mahout#214

--- flink/src/main/scala/org/apache/mahout/flinkbindings/blas/package.scala
+++ flink/src/main/scala/org/apache/mahout/flinkbindings/blas/package.scala
@@ -21,18 +21,27 @@ package org.apache.mahout.flinkbindings
 import java.lang.Iterable
 
 import org.apache.flink.api.common.functions.RichMapPartitionFunction
+import org.apache.flink.api.common.typeinfo.TypeInformation
 import org.apache.flink.api.scala._
+import org.apache.flink.configuration.Configuration
 import org.apache.flink.util.Collector
+import org.apache.mahout.flinkbindings.drm.FlinkDrm
+import org.apache.mahout.math.drm.DrmLike
+import org.apache.mahout.math.{RandomAccessSparseVector, Vector}
+import org.apache.mahout.math._
+import scalabindings._
+import RLikeOps._
 
 import scala.collection._
+import scala.reflect.ClassTag
 
 package object blas {
 
   /**
     * To compute tuples (PartitionIndex, PartitionElementCount)
     *
-    * @param drmDataSet
-    * @tparam K
+    * @param drmDataSet - DRM Dataset
+    * @tparam K - Key type
     * @return (PartitionIndex, PartitionElementCount)
     */
   //TODO: Remove this when FLINK-3657 is merged into Flink codebase and
@@ -47,5 +56,93 @@ package object blas {
         }
       }
     }
+  }
+
+  /**
+    * Rekey matrix dataset keys to consecutive int keys.
+    * @param drmDataSet incoming matrix row-wise dataset
+    * @param computeMap if true, also compute mapping between old and new keys
+    * @tparam K existing key parameter
+    * @return
+    */
+  private[mahout] def rekeySeqInts[K: ClassTag: TypeInformation](drmDataSet: FlinkDrm[K],
+                                                                 computeMap: Boolean = true): (DrmLike[Int],
+    Option[DataSet[(K, Int)]]) = {
+
+    implicit val dc = drmDataSet.context
+
+    val datasetA = drmDataSet.asRowWise.ds
+
+    val ncols = drmDataSet.asRowWise.ncol
+
+    // Flink environment
+    val env = datasetA.getExecutionEnvironment
+
+    // First, compute partition sizes.
+    val partSizes = countsPerPartition(datasetA).collect().toList
+
+    // Starting indices
+    var startInd = new Array[Int](datasetA.getParallelism)
+
+    // Save counts
+    for (pc <- partSizes) startInd(pc._1) = pc._2
+
+    // compute cumulative sum
+    val cumulativeSum = startInd.scanLeft(0)(_ + _).init
+
+    val vector: Vector = new RandomAccessSparseVector(cumulativeSum.length)
+
+    cumulativeSum.indices.foreach { i => vector(i) = cumulativeSum(i).toDouble }
+
+    val bCast = FlinkEngine.drmBroadcast(vector)
+
+    implicit val typeInformation = createTypeInformation[(K, Int)]
+
+    // Compute key -> int index map:
+    val keyMap = if (computeMap) {
+      Some(
+        datasetA.mapPartition(new RichMapPartitionFunction[(K, Vector), (K, Int)] {
+
+          // partition number
+          var part: Int = 0
+
+          // get the index of the partition
+          override def open(params: Configuration): Unit = {
+            part = getRuntimeContext.getIndexOfThisSubtask
+          }
+
+          override def mapPartition(iterable: Iterable[(K, Vector)], collector: Collector[(K, Int)]): Unit = {
+            val k = iterable.iterator().next._1
+            val si = bCast.value.get(part)
+            collector.collect(k -> (part + si).toInt)
+          }
+        }))
+    } else {
+      None
+    }
+
+    // Finally, do the transform
+    val intDataSet = datasetA
+
+      // Re-number each partition
+      .mapPartition(new RichMapPartitionFunction[(K, Vector), (Int, Vector)] {
+
+        // partition number
+        var part: Int = 0
+
+        // get the index of the partition
+        override def open(params: Configuration): Unit = {
+          part = getRuntimeContext.getIndexOfThisSubtask
+        }
+
+        override def mapPartition(iterable: Iterable[(K, Vector)], collector: Collector[(Int, Vector)]): Unit = {
+          val k = iterable.iterator().next._2
+          val si = bCast.value.get(part)
+          collector.collect((part + si).toInt -> k)
+        }
+      })
+
+    // Finally, return drm -> keymap result
+    datasetWrap(intDataSet) -> keyMap
   }
 }
