bf0a461abf32178b476a107b9752facafcb3575b
Andrew Palumbo <apalumbo@apache.org>
2016-04-27 18:35:46 -0400

    initial commit

---
+++ math-scala/src/main/scala/org/apache/mahout/visiualization/plot2d.scala
@@ -0,0 +1,14 @@
+package org.apache.mahout.visiualization
+
+import org.apache.mahout.math._
+import drm._
+import scalabindings._
+import RLikeDrmOps._
+import RLikeOps._
+
+/**
+  * Created by andy on 4/27/16.
+  */
+class plot2d[K](drm: DrmLike[K], samplePercent = 10) {
+     val mPlot: Matrix = drm.
+}
