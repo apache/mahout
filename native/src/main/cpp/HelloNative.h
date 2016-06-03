da98304c1e6fbd750ee1f483ddc08b8700952af7
Andrew Palumbo <apalumbo@apache.org>
2016-06-03 15:27:31 -0400

    wip: inital commit- not compiling

---
+++ native/src/main/cpp/HelloNative.h
@@ -0,0 +1,10 @@
+#include <string>
+
+namespace HelloNative {
+    class HelloNative {
+        public:
+            const std::string& get_property() { return property; }
+            void set_property(const std::string& property) { this->property = property; }
+            std::string property;
+    };
+}
---
+++ native/src/main/scala/org/apache/mahout/javaCppTest/HelloNative.scala
@@ -0,0 +1,35 @@
+package org.apache.mahout.javaCppTest
+
+import org.bytedeco.javacpp._
+import org.bytedeco.javacpp.annotation._
+//
+//@Platform(include="NativeLibrary.h")
+//@Namespace("NativeLibrary")
+
+class HelloNative {
+//  class HelloNative extends Pointer {
+//
+//    Loader.load()
+//
+//    allocate()
+//
+//    native void allocate();
+//
+//    // to call the getter and setter functions
+//    native @StdString String get_property(); public native void set_property(String property);
+//
+//    // to access the member variable directly
+//    native @StdString String property();     public native void property(String property);
+//  }
+//
+////  public static void main( args) {
+////    // Pointer objects allocated in Java get deallocated once they become unreachable,
+////    // but C++ destructors can still be called in a timely fashion with Pointer.deallocate()
+////    NativeClass l = new NativeClass();
+////    l.set_property("Hello World!");
+////    System.out.println(l.property());
+////  }
+}
+object HelloNative {
+//  def {  }
+}
