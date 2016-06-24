6aad9da1b4addfc3e5ccee4ea0662eb7d37f48b0
Andrew Palumbo <apalumbo@apache.org>
2016-06-24 16:03:58 -0400

    removed

--- viennacl/src/main/cpp/vcl_blas3.cpp
+++ viennacl/src/main/cpp/vcl_blas3.cpp
@@ -27,85 +27,6 @@
 
 
 using namespace mmul;
-//
-//    // currently row-major only
-//
-//    // Dense BLAS-3
-//    // dense %*% dense
-//    viennacl::matrix<double> dense_dense_mmul(viennacl::matrix<double> lhs, viennacl::matrix<double> rhs) {
-//     // viennacl::matrix<double> = new viennacl(lhs)
-//      return viennacl::linalg::prod_impl(lhs, rhs);
-//    }
-//    viennacl::matrix<float> dense_dense_mmul(viennacl::matrix<float> lhs, viennacl::matrix<float> rhs) {
-//      return viennacl::linalg::prod(lhs, rhs);
-//    }
-//
-//    // CSR sparse matrices BLAS-3
-//    // dense %*% sparse (CSR)
-//    viennacl::matrix<double> dense_sparse_mmul(viennacl::matrix<double> lhs, viennacl::compressed_matrix<double> rhs) {
-//      return viennacl::linalg::prod(lhs, rhs);
-//    }
-//    viennacl::matrix<float> dense_sparse_mmul(viennacl::matrix<float> lhs, viennacl::compressed_matrix<float> rhs) {
-//      return viennacl::linalg::prod(lhs, rhs);
-//    }
-//
-//    // sparse (CSR) %*% dense
-//    viennacl::matrix<double> sparse_dense_mmul(viennacl::compressed_matrix<double> lhs, viennacl::matrix<double> rhs) {
-//      return viennacl::linalg::prod(lhs, rhs);
-//    }
-//    viennacl::matrix<float> sparse_dense_mmul(viennacl::compressed_matrix<float> lhs, viennacl::matrix<float> rhs) {
-//      return viennacl::linalg::prod(lhs, rhs);
-//    }
-//
-//    // sparse(CSR) %*% sparse (CSR)
-//    viennacl::compressed_matrix<double> sparse_sparse_mmul(viennacl::compressed_matrix<double> lhs, viennacl::compressed_matrix<double> rhs) {
-//      return viennacl::linalg::prod(lhs, rhs);
-//    }
-//    viennacl::compressed_matrix<float> sparse_sparse_mmul(viennacl::compressed_matrix<float> lhs, viennacl::compressed_matrix<float> rhs) {
-//      return viennacl::linalg::prod(lhs, rhs);
-//    }
-//
-//    // sparse (CSR) %*% sparse (COO)
-//    viennacl::compressed_matrix<double> sparse_sparse_mmul(viennacl::compressed_matrix<double> lhs, viennacl::coordinate_matrix<double> rhs) {
-//      return viennacl::linalg::prod(lhs, rhs);
-//    }
-//    viennacl::compressed_matrix<float> sparse_sparse_mmul(viennacl::compressed_matrix<float> lhs, viennacl::coordinate_matrix<float> rhs) {
-//      return viennacl::linalg::prod(lhs, rhs);
-//    }
-//
-//
-//    // COO sparse matrices BLAS-3
-//    // dense %*% sparse (COO)
-//    viennacl::matrix<double> dense_sparse_mmul(viennacl::matrix<double> lhs, viennacl::coordinate_matrix<double> rhs) {
-//      return viennacl::linalg::prod(lhs, rhs);
-//    }
-//    viennacl::matrix<float> dense_sparse_mmul(viennacl::matrix<float> lhs, viennacl::coordinate_matrix<float> rhs) {
-//      return viennacl::linalg::prod(lhs, rhs);
-//    }
-//
-//    // sparse (COO) %*% dense
-//    viennacl::matrix<double> sparse_dense_mmul(viennacl::coordinate_matrix<double> lhs, viennacl::matrix<double> rhs) {
-//      return viennacl::linalg::prod(lhs, rhs);
-//    }
-//    viennacl::matrix<float> sparse_dense_mmul(viennacl::coordinate_matrix<float> lhs, viennacl::matrix<float> rhs) {
-//      return viennacl::linalg::prod(lhs, rhs);
-//    }
-//
-//    // sparse (COO) %*% sparse (COO)
-//    viennacl::coordinate_matrix<double> sparse_sparse_mmul(viennacl::coordinate_matrix<double> lhs, viennacl::coordinate_matrix<double> rhs) {
-//     return viennacl::linalg::prod(lhs, rhs);
-//    }
-//    viennacl::coordinate_matrix<float> sparse_sparse_mmul(viennacl::coordinate_matrix<float> lhs, viennacl::coordinate_matrix<float> rhs) {
-//      return viennacl::linalg::prod(lhs, rhs);
-//    }
-//
-//    // sparse (COO) %*% sparse (CSR)
-//    viennacl::coordinate_matrix<double> sparse_sparse_mmul(viennacl::coordinate_matrix<double> lhs, viennacl::compressed_matrix<double> rhs) {
-//      return viennacl::linalg::prod(lhs, rhs);
-//    }
-//    viennacl::coordinate_matrix<float> sparse_sparse_mmul(viennacl::coordinate_matrix<float> lhs, viennacl::compressed_matrix<float> rhs) {
-//       return viennacl::linalg::prod(lhs, rhs);
-//    }
 
     // bridge to JNI functions
 
@@ -122,14 +43,5 @@ using namespace mmul;
 
     }
 
-//    float* dense_dense_mmul(float* lhs, float* rhs) {
-//      viennacl::matrix mx_a = new viennacl::matrix<double>(lhs, viennacl::memory_types::main_memory, lhs_rows, lhs_cols);
-//      viennacl::matrix mx_b = new viennacl::matrix<double>(rhs, viennacl::memory_types::main_memory, rhs_rows, rhs_cols);
-//
-//      viennacl::matrix res = dense_dense_mmul(mx_a, mx_b);
-//
-//      return res.
-//
-//    }
 
 
--- viennacl/src/main/cpp/vcl_blas3.h
+++ viennacl/src/main/cpp/vcl_blas3.h
@@ -23,66 +23,18 @@
 
 namespace mmul {
 
-    // currently row-major only
-
-    // Dense BLAS-3
-    // dense %*% dense
-//    void dense_dense_mmul(double* lhs, long lhs_rows, long lhs_cols, double* rhs, long rhs_rows, long rhs_cols, double* result)
-   // viennacl::matrix<float> dense_dense_mmul(viennacl::matrix<float> lhs, viennacl::matrix<float> rhs);
-
-    // CSR sparse matrices BLAS-3
-    // dense %*% sparse (CSR)
-//    viennacl::matrix<double> dense_sparse_mmul(viennacl::matrix<double> lhs, viennacl::compressed_matrix<double> rhs);
-//    viennacl::matrix<float> dense_sparse_mmul(viennacl::matrix<float> lhs, viennacl::compressed_matrix<float> rhs);
-
-    // sparse (CSR) %*% dense
-//    viennacl::matrix<double> sparse_dense_mmul(viennacl::compressed_matrix<double> lhs, viennacl::matrix<double> rhs);
-//    viennacl::matrix<float> sparse_dense_mmul(viennacl::compressed_matrix<float> lhs, viennacl::matrix<float> rhs);
-
-    // sparse(CSR) %*% sparse (CSR)
-//    viennacl::compressed_matrix<double> sparse_sparse_mmul(viennacl::compressed_matrix<double> lhs, viennacl::compressed_matrix<double> rhs);
-//    viennacl::compressed_matrix<float> sparse_sparse_mmul(viennacl::compressed_matrix<float> lhs, viennacl::compressed_matrix<float> rhs);
-
-    // sparse (CSR) %*% sparse (COO)
-//    viennacl::compressed_matrix<double> sparse_sparse_mmul(viennacl::compressed_matrix<double> lhs, viennacl::coordinate_matrix<double> rhs);
-//    viennacl::compressed_matrix<float> sparse_sparse_mmul(viennacl::compressed_matrix<float> lhs, viennacl::coordinate_matrix<float> rhs);
-
-
-
-    // COO sparse matrices BLAS-3
-    // dense %*% sparse (COO)
-//    viennacl::matrix<double> dense_sparse_mmul(viennacl::matrix<double> lhs, viennacl::coordinate_matrix<double> rhs);
-//    viennacl::matrix<float> dense_sparse_mmul(viennacl::matrix<float> lhs, viennacl::coordinate_matrix<float> rhs);
-
-    // sparse (COO) %*% dense
-//    viennacl::matrix<double> sparse_dense_mmul(viennacl::coordinate_matrix<double> lhs, viennacl::matrix<double> rhs);
-//    viennacl::matrix<float> sparse_dense_mmul(viennacl::coordinate_matrix<float> lhs, viennacl::matrix<float> rhs);
-
-    // sparse (COO) %*% sparse (COO)
-//    viennacl::coordinate_matrix<double> sparse_sparse_mmul(viennacl::coordinate_matrix<double> lhs, viennacl::coordinate_matrix<double> rhs);
-//    viennacl::coordinate_matrix<float> sparse_sparse_mmul(viennacl::coordinate_matrix<float> lhs, viennacl::coordinate_matrix<float> rhs);
-
-    // sparse (COO) %*% sparse (CSR)
-//    viennacl::coordinate_matrix<double> sparse_sparse_mmul(viennacl::coordinate_matrix<double> lhs, viennacl::compressed_matrix<double> rhs);
-//    viennacl::coordinate_matrix<float> sparse_sparse_mmul(viennacl::coordinate_matrix<float> lhs, viennacl::compressed_matrix<float> rhs);
-
-
-    // bridge to JNI functions
+     // bridge to JNI functions
 
     // CSR sparse matrices BLAS-3
     // dense %*% dense
      void dense_dense_mmul(double* lhs, long lhs_rows, long lhs_cols, double* rhs, long rhs_rows, long rhs_cols, double* result)
-//    float* dense_dense_mmul(double* lhs, long lhs_rows, long lhs_cols, double* rhs, long rhs_rows, long rhs_cols);
 
-    // dense %*% sparse (CSR)/(COO) with matrix from memory/std matrix preperation
-//    void dense_sparse_mmul(double* lhs, long lhs_rows, long lhs_cols, std::vector<std::vector<double> > rhs, bool isRhsCSR);
-//    float* dense_sparse_mmul(float * lhs, long lhs_rows, long lhs_cols, std::vector<std::vector<float> > rhs, bool isRhsCSR);
+    // dense %*% sparse (CSR)/ with matrix from memory/std matrix preperation
+//    void dense_sparse_mmul(double* lhs, long lhs_rows, long lhs_cols, std::vector<std::vector<double> > rhs);
 
-    // sparse (CSR)/(COO) %*% dense
-//    void  sparse_dense_mmul(std::vector<std::vector<double> > lhs, double* rhs, long rhs_rows, long rhs_cols);
-//    float*  sparse_dense_mmul(std::vector<std::vector<float> > lhs, float* rhs, long rhs_rows, long rhs_cols);
+    // sparse (CSR) %*% dense
+//     void  sparse_dense_mmul(std::vector<std::vector<double> > lhs, double* rhs, long rhs_rows, long rhs_cols, double* result);
 
-    // sparse (CSR)/(COO) %*% sparse (CSR)/(COO)
-//    std::vector<std::vector<double> > sparse_sparse_mmul(std::vector<std::vector<double> > lhs, bool isLhsCsr, std::vector<std::vector<double> >, bool isRhsCsr);
-//    std::vector<std::vector<double> > sparse_sparse_mmul(std::vector<std::vector<float> > lhs, bool isLhsCsr, std::vector<std::vector<float> > rhs,  bool isRhsCsr);
+     // sparse (CSR) %*% sparse (CSR)
+//     void  sparse_sparse_mmul(std::vector<std::vector<double> >* lhs, std::vector<std::vector<double> >* rhs, std::vector<std::vector<double> >* res);
 }
