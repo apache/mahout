25bf0fbf810d039996a35da13202c513e7034e2a
mzjgithub <mu_zhongjiang@163.com>
2017-04-19 18:16:36 +0800

    add Serializable

--- mr/src/main/java/org/apache/mahout/common/parameters/Parametered.java
+++ mr/src/main/java/org/apache/mahout/common/parameters/Parametered.java
@@ -17,12 +17,12 @@
 
 package org.apache.mahout.common.parameters;
 
+import java.util.Collection;
+
 import org.apache.hadoop.conf.Configuration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.io.Serializable;
-import java.util.Collection;
 
 /** Meta information and accessors for configuring a job. */
 public interface Parametered extends Serializable{
