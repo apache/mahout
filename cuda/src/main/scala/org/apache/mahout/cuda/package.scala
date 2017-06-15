d173d01e11f79991778b82879ec35fc61199eac1
Andrew Palumbo <apalumbo@apache.org>
2017-06-14 18:43:09 -0700

    Finished sparse %*% dense MMul

--- cuda/src/main/scala/org/apache/mahout/cuda/package.scala
+++ cuda/src/main/scala/org/apache/mahout/cuda/package.scala
@@ -322,11 +322,11 @@ package object cuda {
       b.descr, b.nonz,
       b.vals, b.row_ptr, b.col_ind,
       c.descr,
-      c.vals, c.row_ptr, c.col_ind);
+      c.vals, c.row_ptr, c.col_ind)
     c
   }
 
-  def prod(a: CompressedMatrix, b: DenseRowMatrix, ctx: Context): CompressedMatrix = {
+  def prod(a: CompressedMatrix, b: DenseRowMatrix, ctx: Context): DenseRowMatrix = {
     var m = a.nrows
     var n = b.ncols
     var k = b.nrows
@@ -338,25 +338,14 @@ package object cuda {
     alpha(0) = 1.0d
     beta(0) = 0.0d
 
-//    cusparseDcsrmm(cusparseHandle_t handle,
-//      cusparseOperation_t transA, int m, int n, int k,
-//      int nnz,
-//      const double *alpha,
-//      const cusparseMatDescr_t descrA,
-//      const double *csrValA,
-//      const int *csrRowPtrA,
-//      const int *csrColIndA,
-//      const double *B,
-//      int ldb,
-//      const double *beta,
-//      double *C, int ldc)
-
     cusparseDcsrmm(ctx.sparseHandle, a.trans, m, n, k,
-      a.nonz, jcuda.Pointer.to(alpha), a.descr,
+      a.nonz,
+      jcuda.Pointer.to(alpha),
+      a.descr,
       a.vals, a.row_ptr, a.col_ind,
-      a.descr, a.nonz,
       b.vals, n,
-      jcuda.Pointer.to(beta), c.vals, k);
+      jcuda.Pointer.to(beta),
+      c.vals, k)
     c
   }
 
--- cuda/src/test/scala/org/apache/mahout/cuda/CUDATestSuite.scala
+++ cuda/src/test/scala/org/apache/mahout/cuda/CUDATestSuite.scala
@@ -23,6 +23,10 @@ class CUDATestSuite extends FunSuite with Matchers {
   test("dense mmul at geometry of 1000 x 1000 %*% 1000 x 1000"){
     CUDATestSuite.getAverageTimeDense(1000, 1000, 1000, 5)
   }
+
+  test("dense mmul %*% sparse at geometry of 1000 x 1000 %*% 1000 x 1000"){
+    CUDATestSuite.getAverageTimeDense(1000, 1000, 1000, 5)
+  }
 }
 
 
@@ -131,6 +135,61 @@ object CUDATestSuite {
     cudaB.close()
     mxCuda.close()
 
+    ms
+  }
+
+  def getAverageTimeSparseDense(m: Int = 1000,
+                           s: Int = 1000,
+                           n: Int = 1000,
+                           density: Double = .2,
+                           seed: Long = 1234L,
+                           nruns: Int = 5): Long = {
+
+    val r = new Random(seed)
+    val cudaCtx = new Context()
+
+    // sparse row-wise
+    val mxA = new SparseRowMatrix(m, s, false)
+    val mxB = new DenseMatrix(s, n)
+
+    // add some sparse data with the given threshold for sparse and set all values
+    // for dense.
+    mxA := { (_, _, v) => if (r.nextDouble() < density) r.nextDouble() else v }
+    mxB := { (_, _, v) => r.nextDouble() }
+
+    // run Mahout JVM - only math once
+    var mxC = mxA %*% mxB
+
+    // run Mahout JVM - only math another {{nruns}} times and take average
+    var ms = System.currentTimeMillis()
+    for (i: Int  <- 1 to nruns) {
+      mxC = mxA %*% mxB
+    }
+    ms = (System.currentTimeMillis() - ms) / nruns
+    print(s"Mahout JVM Sparse multiplication time: $ms ms.\n")
+
+
+    // run Mahout JCuda math bindings once
+    val cudaA = toCudaCmpMatrix(mxA, cudaCtx)
+    val cudaB = toCudaDenseRM(mxB, cudaCtx)
+    var mxCuda = prod(cudaA, cudaB, cudaCtx)
+
+    // run Mahout JCuda another {{nruns}} times and take average
+    ms = System.currentTimeMillis()
+    for (i: Int  <- 1 to nruns) {
+      mxCuda = prod(cudaA, cudaB, cudaCtx)
+    }
+
+    ms = (System.currentTimeMillis() - ms) / nruns
+    print(s"Mahout JCuda Sparse multiplication time: $ms ms.\n")
+
+    // TODO: Ensure that we've been working with the same matrices.
+    // (mxC - mxCuda).norm / mxC.nrow / mxC.ncol should be < 1e-16
+
+    assert(((mxC - fromCudaDenseRM(mxCuda, cudaCtx)).norm / mxC.nrow / mxC.ncol) < 1e-16)
+    cudaA.close()
+    cudaB.close()
+    mxCuda.close()
     ms
   }
 
