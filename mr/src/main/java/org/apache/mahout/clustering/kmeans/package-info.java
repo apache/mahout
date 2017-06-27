99a5358f31b15c982f9a34cafbf496271562f851
dustinvanstee <dustinvanstee@gmail.com>
2017-06-27 11:08:45 -0400

    removed all files except for website directory

--- mr/src/main/java/org/apache/mahout/clustering/kmeans/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * This package provides an implementation of the <a href="http://en.wikipedia.org/wiki/Kmeans">k-means</a> clustering
- * algorithm.
- */
-package org.apache.mahout.clustering.kmeans;
