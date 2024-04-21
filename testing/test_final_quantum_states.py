214e202e647ddb3ac49bbb667e801812f0755b29
Tommy Naugle <tdnaugle@gmail.com>
2024-04-21 17:22:47 -0500

    fixes and tidying

--- testing/test_final_quantum_states.py
+++ testing/test_final_quantum_states.py
@@ -2,11 +2,9 @@ from qumat_helpers import get_qumat_example_final_state_vector
 import numpy as np
 from importlib import import_module
 
-# Placeholder test, will delete
 def test_final_state_vector():
     # Specify initial computational basis state vector
     initial_ket_str = "001"
-    # Execute qumat and native implementations
 
     backends_to_test = ["qiskit"]
     for backend_name in backends_to_test:
@@ -18,7 +16,6 @@ def test_final_state_vector():
         qumat_backend_config = backend_module.get_qumat_backend_config("get_final_state_vector")
         qumat_example_vector = get_qumat_example_final_state_vector(qumat_backend_config, initial_ket_str)
 
-
-    # Compare final state vectors from qumat vs. native implementation
-    np.testing.assert_array_equal(qumat_example_vector, native_example_vector)
+        # Compare final state vectors from qumat vs. native implementation
+        np.testing.assert_array_equal(qumat_example_vector, native_example_vector)
 
