138ca640a5174ead87ab88f0d8c3e68381b258cc
400Ping <fourhundredping@gmail.com>
2026-01-26 19:24:22 +0800

    fix
    Signed-off-by: 400Ping <fourhundredping@gmail.com>

--- website/src/theme/Blog/Components/Author/index.tsx
+++ website/src/theme/Blog/Components/Author/index.tsx
@@ -1,10 +1,3 @@
-/**
- * Copyright (c) Facebook, Inc. and its affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
 import React, {type ReactNode} from 'react';
 import clsx from 'clsx';
 import Link, {type Props as LinkProps} from '@docusaurus/Link';
