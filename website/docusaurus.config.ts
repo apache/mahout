dff4836579a9efb1d363db7c1397336e99113ba5
Yu-Ting Hsiung <bear890707@gmail.com>
2026-03-07 11:42:07 +0800

    docs: self host katex.min.css to fix not loading issue

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -89,10 +89,8 @@ const config: Config = {
   // KaTeX stylesheet for math rendering
   stylesheets: [
     {
-      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+      href: '/css/katex.min.css',
       type: 'text/css',
-      integrity: 'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
-      crossorigin: 'anonymous',
     },
   ],
 
